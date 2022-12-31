422d2f97990e3e53e3281105e07a336bf4786070
Mounir Benzid <m.benzid@douglas.de>
2022-12-31 23:00:21 +0100

    working with traits

--- src/main/scala/com/mobe/zioplayground/Main.scala
+++ src/main/scala/com/mobe/zioplayground/Main.scala
@@ -1,34 +1,56 @@
 package com.mobe
 package zioplayground
 
-import com.mobe.zioplayground.Constants.*
-import com.mobe.zioplayground.TwoDimensionalStr
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
-import scala.annotation.targetName
-import scala.concurrent.duration.{DurationInt, FiniteDuration}
-
-def showTime:Unit =
-  Terminal.clearTerminal
-  Terminal.goUp(LinesToGoUp)
-  TwoDimensionalTime.showCurrent(TimePattern)
-
-object TickUntilEnterIsPressed:
-
+object Main:
   def main(args: Array[String]): Unit =
-    Scala.tickUntilEnterIsPressed(interval = Interval1Sec) {showTime}
+    println("-" * 30)
+    code(args)
+    println("-" * 30)
 
 
-object TickForFewSeconds:
+  def code(args: Array[String]): Unit =
+    class Animal
+    trait Pet:
+      def allowedToSleepInBed: Boolean
+      def disAllowedToSleepInBed: Boolean = ! allowedToSleepInBed
+    class Cat extends Animal with Pet:
+      val allowedToSleepInBed = true
+    class Turtle extends Animal with Pet:
+      val allowedToSleepInBed = false
 
-  def main(args: Array[String]): Unit =
-    Scala.loop(times = FewSeconds) {
-      showTime
-      Scala.wait(Interval1Sec)
-    }
+    def show(pet: Pet): Unit =
+      println(pet.disAllowedToSleepInBed)
 
-object Constants:
+//    show(new Cat)
+//    show(new Turtle)
 
-  val FewSeconds: Int = 3
-  val TimePattern: String = "HH:mm:ss"
-  val Interval1Sec: FiniteDuration = 1.second
-  val LinesToGoUp: Int = Int.MaxValue
+    trait Timestamp:
+      val creationTime: String =
+        lazy val formatter: DateTimeFormatter | Null = java.time.format.DateTimeFormatter.ofPattern("HH:mm:ss")
+        val now: LocalDateTime | Null = java.time.LocalDateTime.now()
+        if (now == null || formatter == null) ""
+        else
+          val time = now.format(formatter)
+          if (time == null) "" else time
+    end Timestamp
+
+
+    val path = "/Users/m.benzid/Projects/zioplayground/src/main/scala/com/mobe/zioplayground/Main.scala"
+    val file: File & Timestamp = new File(path) with Timestamp
+
+    def showName(file: File): Unit =
+      println(s"file.getName = ${file.getName}")
+
+    def showCreationTime(timestamp: Timestamp): Unit =
+      println(s"file.creationTime = ${timestamp.creationTime}")
+
+    showName(file)
+    showCreationTime(file)
+
+  end code
+end Main
+
--- src/main/scala/com/mobe/zioplayground/Terminal.scala
+++
@@ -1,6 +0,0 @@
-package com.mobe.zioplayground
-
-object Terminal:
-  def clearTerminal: Unit = print("\u001b[2J")
-
-  def goUp(linesToGoUp: Int): Unit = print(s"\u001b[${linesToGoUp}A")
