30f393acb810e444c3ec3de7eb49a58d0414a682
Mounir Benzid <m.benzid@douglas.de>
2023-01-01 19:45:20 +0100

    pattern matching / case classes https://www.youtube.com/watch?v=oOj7rfoDzJ0&list=PLJGDHERh23x-YBJ8LmYU_IGBFflvsKfLu&index=24

--- src/main/scala/com/mobe/zioplayground/Main.scala
+++ src/main/scala/com/mobe/zioplayground/Main.scala
@@ -1,19 +1,65 @@
 package com.mobe
 package zioplayground
 
-import java.io.File
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
+import java.awt.Button
+import java.util.EventListener
+
+
+
+
 
 object Main:
+
+  object File2:
+
+    def apply(location: String, name: String, ext: String) = s"$location/$name.$ext"
+
+    def unapply(path: String): Option[(String, String, String)] =
+
+      val lastDotPos = path.lastIndexOf(".")
+      val lastSlashPos = path.lastIndexOf("/")
+      val ext = path.substring(lastDotPos + 1)
+      val name = path.substring(lastSlashPos + 1, lastDotPos)
+      val location = path.substring(0, lastSlashPos)
+
+      if (path.isBlank || location == null || name == null || ext == null)
+        None
+      else
+        Some((
+          location,
+          name,
+          ext
+        ))
+    end unapply
+  end File2
+
+
+  case class File(location: String, name: String, ext: String)
+
+
   def main(args: Array[String]): Unit =
     println("-" * 30)
     code(args)
     println("-" * 30)
 
   def code(args: Array[String]): Unit =
-//    println(new Lambo("Murcileago"))
-    println(new BMW("M3 GTR: 220 / 2 + 3 ="))
+
+    val explicitFile: String = "/Users/m.benzid/Projects/zioplayground/src/main/scala/com/mobe/zioplayground/Main.scala"
+
+    val implicitFile: File =
+      File(
+        location = "/Users/m.benzid/Projects/zioplayground/src/main/scala/com/mobe/zioplayground",
+        name = "Main",
+        ext = "scala",
+      )
+
+    println("explicit: " + explicitFile)
+    println("implicit: " + implicitFile)
+
+    val x =File.apply
+    val y = x("1", "2", "3")
+
+
   end code
 end Main
 
