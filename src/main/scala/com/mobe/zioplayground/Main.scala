d02cdc13a88972ed8e977538c35f89f1650fef16
Mounir Benzid <m.benzid@douglas.de>
2023-01-02 09:03:23 +0100

    exception handling

--- src/main/scala/com/mobe/zioplayground/Main.scala
+++ src/main/scala/com/mobe/zioplayground/Main.scala
@@ -1,41 +1,28 @@
 package com.mobe
 package zioplayground
 
+import com.mobe.zioplayground.Write.*
+
 import java.awt.Button
 import java.util.EventListener
 
+object Helpers:
 
+  def showRed(string: String): Unit = styled(string, Console.RED)
 
+  def showYellow(string: String): Unit = styled(string, Console.YELLOW)
+
+  def showGreen(string: String): Unit = styled(string, Console.GREEN)
+
+  private def styled(string: String, color: String): String =
+    println(color + string + Console.RESET)
+    ""
 
 
 object Main:
 
-  object File2:
-
-    def apply(location: String, name: String, ext: String) = s"$location/$name.$ext"
-
-    def unapply(path: String): Option[(String, String, String)] =
-
-      val lastDotPos = path.lastIndexOf(".")
-      val lastSlashPos = path.lastIndexOf("/")
-      val ext = path.substring(lastDotPos + 1)
-      val name = path.substring(lastSlashPos + 1, lastDotPos)
-      val location = path.substring(0, lastSlashPos)
-
-      if (path.isBlank || location == null || name == null || ext == null)
-        None
-      else
-        Some((
-          location,
-          name,
-          ext
-        ))
-    end unapply
-  end File2
-
-
-  case class File(location: String, name: String, ext: String)
-
+  import Helpers.*
+  import com.mobe.zioplayground.MyFile
 
   def main(args: Array[String]): Unit =
     println("-" * 30)
@@ -43,22 +30,36 @@ object Main:
     println("-" * 30)
 
   def code(args: Array[String]): Unit =
+    showRed("Red")
+    showYellow("Yellow")
+    showGreen("Green")
 
-    val explicitFile: String = "/Users/m.benzid/Projects/zioplayground/src/main/scala/com/mobe/zioplayground/Main.scala"
+    val file = new MyFile(
+      location = "/Users/m.benzid/Projects/zioplayground/src/main/scala/com/mobe/zioplayground",
+      name = "Main",
+      extension = "scala",
+      content = "Blah")
 
-    val implicitFile: File =
-      File(
-        location = "/Users/m.benzid/Projects/zioplayground/src/main/scala/com/mobe/zioplayground",
-        name = "Main",
-        ext = "scala",
-      )
 
-    println("explicit: " + explicitFile)
-    println("implicit: " + implicitFile)
+    val diskUsage:Int = 10
 
-    val x =File.apply
-    val y = x("1", "2", "3")
+    val newDiskUsage: Int =
+      try
+       file.write() match
+         case Success(size) =>
+           diskUsage + size
+         case _ => diskUsage
+      catch
+        case Error(message) =>
+          showRed(s"Warning: $message")
+          diskUsage
+        case Warning(message) =>
+          showRed(s"Warning: $message")
+          diskUsage
+      end try
 
+
+    println(s"diskUsage = $newDiskUsage bytes")
 
   end code
 end Main
